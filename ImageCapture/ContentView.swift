e91a64f8f2fc113bbe321e71ef87d8e96ce05605
Bandu Wewalaarachchi <bandu@ecyber.com>
2019-12-29 11:20:08 +0800

    Initial Commit

---
+++ ImageCapture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageCapture
+//
+//  Created by Bandu Wewalaarachchi on 29/12/19.
+//  Copyright © 2019 Bandu Wewalaarachchi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
